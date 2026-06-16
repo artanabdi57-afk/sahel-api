851532937de6b63f1f996b01938c99bf3ff46783
artanabdi57-afk <artanabdi57@gmail.com>
2026-06-16 22:44:53 +0300

    add vercel cors

--- src/app.js
+++ src/app.js
@@ -20,10 +20,9 @@ const authenticate = require("./middleware/authMiddleware");
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://127.0.0.1:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:3000"
+  'http://localhost:5173',
+  'http://127.0.0.1:5173',
+  'https://sahel-api.vercel.app',
 ];
 
 const isLocalOrigin = (origin) => {
