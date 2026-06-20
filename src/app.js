7a6519202c12a2d600ffc6bec17438c0d2b8bdd6
artanabdi57-afk <artanabdi57@gmail.com>
2026-06-20 19:39:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -70,7 +70,7 @@ app.use(
       return callback(new Error("Not allowed by CORS"));
     },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", "x-owner-code"]
   })
 );
 
