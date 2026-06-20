40cdc6f544850f089fb9dcc65add29a20eef9c9d
artanabdi57-afk <artanabdi57@gmail.com>
2026-06-20 06:47:02 +0300

    Update creditRoutes.js

--- src/routes/creditRoutes.js
+++ src/routes/creditRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const {
+  createCredit,
   getCredits,
   getCreditSummary,
   markCreditPaid,
   recordPartialPayment
 } = require("../controllers/creditController");
-
 const router = express.Router();
-
 router.get("/", getCredits);
 router.get("/summary", getCreditSummary);
+router.post("/", createCredit);
 router.put("/:id/paid", markCreditPaid);
 router.put("/:id/partial", recordPartialPayment);
-
 module.exports = router;
