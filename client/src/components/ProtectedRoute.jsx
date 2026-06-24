0033ef1ccdb810ca747904399d4131c358f728d5
artanabdi57-afk <artanabdi57@gmail.com>
2026-06-24 23:25:09 +0300

    Update ProtectedRoute.jsx

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -6,8 +6,7 @@ export default function ProtectedRoute() {
   const location = useLocation();
 
   if (!isAuthenticated()) {
-    return <Navigate to="/login" replace state={{ from: location.pathname }} />;
-  }
+  return <Navigate to="/welcome" replace state={{ from: location.pathname }} />;
 
   if (!getCurrentShop() && location.pathname !== "/onboarding") {
     return <Navigate to="/onboarding" replace />;
