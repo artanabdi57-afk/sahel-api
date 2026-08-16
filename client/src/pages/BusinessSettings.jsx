6f042538684851b9a1b956e880a4dcac3dfbcf6c
artanabdi57-afk <artanabdi57@gmail.com>
2026-08-16 15:43:54 +0300

    Route hospital accounts to advanced settings

--- client/src/pages/BusinessSettings.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { getCurrentShop } from "../lib/auth";
-import Settings from "./Settings.jsx";
-import HospitalSettings from "./hospital/HospitalSettings.jsx";
-
-export default function BusinessSettings() {
-  const shop = getCurrentShop();
-  return shop?.business_type === "hospital" ? <HospitalSettings /> : <Settings />;
-}
